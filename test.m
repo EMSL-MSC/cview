2ab6d140c0245afc3b58c52fc3ec3f7f5a8120c3
berwin <brock.erwin@pnnl.gov>
2011-02-07 22:29:49 +0000

    added comment

--- test.m
+++ test.m
@@ -9,6 +9,10 @@ int main(int argc, char *argv[], char *env[]) {
 	Class c;
 	c = NSClassFromString(@"MYClass");
 
+	Class foo;
+	// UNCOMMENT the following line, and you will see the bug dissappear...
+	//foo = [MYClass class];
+
 	if ( c == nil )
 		NSLog(@" NSClassFromString returned nil -- this is broken ! ");
 	else
