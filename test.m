8b064798ce908f819a2a364fcd05fa22e08e9697
berwin <brock.erwin@pnnl.gov>
2011-04-07 21:03:11 +0000

    able to easily test multiple urls by passing as argv[1]

--- test.m
+++ test.m
@@ -7,10 +7,20 @@
 
 int main(int argc, char *argv[], char *env[]) {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	NSURL *dataURL = [[NSURL URLWithString: @"http://www.google.com"] retain];
+	NSString *url;
+	if(argc == 2)
+		url = [NSString stringWithCString: argv[1]];
+	else
+		url = @"http://xander.emsl.pnl.gov/cluster/emslfs/cpu_user.data";
+
+	NSURL *dataURL = [[NSURL URLWithString: url] retain];
 	Class handlerClass = [NSURLHandle URLHandleClassForURL: dataURL];
 	NSURLHandle *X = [[handlerClass alloc] initWithURL: dataURL cached: YES];
+	NSLog(@"before loadInForeground");
 	NSData *Xt = [X loadInForeground];
-	fwrite([Xt bytes], [Xt length], 1, stdout);
+	FILE* foo = fopen("foo", "w");
+	fwrite([Xt bytes], [Xt length], 1, foo);
+	fclose(foo);
+
 	return 0;
 }
